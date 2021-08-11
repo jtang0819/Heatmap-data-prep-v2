d95ff2337058f79d1e23b083695ccf4e1007fdac
Jordan <jtang0819@gmail.com>
2021-08-11 01:13:32 -0400

    init commit

---
+++ convert_to_long_lat.py
@@ -0,0 +1,24 @@
+import time
+from googlemaps import Client as GoogleMaps
+import pandas as pd
+from credentials import api_key
+
+def transform():
+    gmaps = GoogleMaps(api_key)
+    addresses = pd.read_csv("addresses.csv")
+    addresses['long'] = ""
+    addresses['lat'] = ""
+    for x in range(len(addresses)):
+        try:
+            time.sleep(1)  # to add delay in case of large DFs
+            geocode_result = gmaps.geocode(addresses['Address'][x])
+            addresses['lat'][x] = geocode_result[0]['geometry']['location']['lat']
+            addresses['long'][x] = geocode_result[0]['geometry']['location']['lng']
+            print(addresses['lat'][x], addresses['long'][x])
+        except IndexError:
+            print("Address was wrong...")
+        except Exception as e:
+            print("Unexpected error occurred.", e)
+    # addresses.head()
+    addresses.to_csv('address_coords.csv')
+    return print(addresses.head())
---
+++ main.py
@@ -0,0 +1,8 @@
+from convert_to_long_lat import transform
+
+if __name__ == '__main__':
+    print("Program Start")
+    print("Returning long and lat from addresses")
+    transform()
+    print("Program Complete, new file address_coords.csv")
+
